5aff590cb19faf804e94d64e808c7f3c6bca6f43
Gilbert <ktf2002kzz@gmail.com>
2018-11-18 19:07:40 +0900

    initial commit

---
+++ baekjoon1978/main.c
@@ -0,0 +1,39 @@
+//
+//  main.c
+//  baekjoon1978
+//
+//  Created by 이승섭 on 2018. 11. 15..
+//  Copyright © 2018년 이승섭. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int N, i;
+    int arr[1000];
+    int count = 0;
+    
+    scanf("%d", &N);
+    
+    if (0 <= N && N <= 100)
+    {
+        for(i = 0; i < N; i++)
+            scanf("%d", &arr[i]);
+    }
+    for(i = 0; i < N; i++)
+    {
+        if(arr[i] == 0 || arr[i] == 1)
+            continue;
+        else if(arr[i] == 3)
+            count++;
+        else if(arr[i] % 2 == 0 || arr[i] % 3 == 0)
+            continue;
+        else
+            count++;
+    }
+    
+    printf("%d \n", count);
+    
+    return 0;
+}
